d8aef66f8c455b06a9d2ef58dd19f612a8830f10
sakataalan <sakataalan@yahoo.com.br>
2022-10-14 17:17:44 -0300

    first commit

--- primeiro-projeto-intellij-java/src/br/com/dio/PrimeiroPrograma.java
+++ primeiro-projeto-intellij-java/src/br/com/dio/PrimeiroPrograma.java
@@ -6,6 +6,6 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato1 = new Gato("gato1", "preto", 2);
         Gato gato2 = new Gato("gato2", "preto", 3);
-        System.out.println(gato1.equals(gato2 ));
+        System.out.println(gato1.equals(gato2));
     }
 }
